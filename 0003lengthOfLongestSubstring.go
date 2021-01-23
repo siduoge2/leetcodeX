cfea3c0a8adc656e23778e894ef9d41148ac1f31
siduoge2 <631851810@qq.com>
2021-01-24 00:13:15 +0800

    add 0003

--- 0003lengthOfLongestSubstring.go
+++ 0003lengthOfLongestSubstring.go
@@ -1,5 +1,59 @@
 package leecode
 
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: s = "abcabcbb"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+//
+//
+// 示例 2:
+//
+//
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+//
+//
+// 示例 3:
+//
+//
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+//
+//
+// 示例 4:
+//
+//
+//输入: s = ""
+//输出: 0
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= s.length <= 5 * 104
+// s 由英文字母、数字、符号和空格组成
+//
+// Related Topics 哈希表 双指针 字符串 Sliding Window
+// 👍 4852 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//func lengthOfLongestSubstring(s string) int {
+//
+//}
+//leetcode submit region end(Prohibit modification and deletion)
+
 // 输入: "pwwkew"
 // 输出: 3
 // 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
@@ -9,7 +63,7 @@ package leecode
 	fmt.Print(lengthOfLongestSubstring(str))
 }*/
 
-func lengthOfLongestSubstring(s string) int {
+func lengthOfLongestSubstring1(s string) int {
 	var csc []byte
 	var ml int
 	for i := 0; i < len(s); i++ {
@@ -36,7 +90,7 @@ func lengthOfLongestSubstring(s string) int {
 	return ml
 }
 
-/*func lengthOfLongestSubstring(s string) int {
+func lengthOfLongestSubstring2(s string) int {
 	arrString := [1000]int{}
 	indexOfArr := 0
 	maxLen := 0
@@ -79,4 +133,4 @@ func lenOfArrString(strings [1000]int) int {
 		}
 	}
 	return sum
-}*/
+}
